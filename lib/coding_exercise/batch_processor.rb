b23f5d25bcaa3d55cc44d0709d40901356c14840
Chris Atkins <christopherlionelatkins@gmail.com>
2014-09-26 23:36:05 +1000

    filled in batch processor class

--- lib/coding_exercise/batch_processor.rb
+++ lib/coding_exercise/batch_processor.rb
@@ -1,3 +1,22 @@
-class BatchProcessor < Struct.new(:destination_parser, :taxonomy_parser, :template)
+class BatchProcessor
 
+  attr_reader :destination_parser, :taxonomy_parser, :renderer
+
+  def initialize(options = {})
+    @destination_parser = options.fetch :destination_parser
+    @taxonomy_parser    = options.fetch :taxonomy_parser
+    @renderer           = options.fetch :renderer
+  end
+
+  def call(&block)
+    taxonomy_parser.traverse do |node|
+      destination = find_destination_info node
+      html = renderer.render destination, node
+      yield html, destination
+    end
+  end
+
+  def find_destination_info(taxonomy_node)
+    destination_parser.find_by_atlas_id taxonomy_node.atlas_id
+  end
 end
--- spec/batch_processor_spec.rb
+++ spec/batch_processor_spec.rb
@@ -1,7 +1,22 @@
 require 'spec_helper'
 
 describe BatchProcessor do
-  it { is_expected.to respond_to :template }
+  let(:renderer) { instance_double 'Renderer' }
+  let(:destination_parser) { instance_double 'DestinationParser' }
+  let(:taxonomy_parser) { instance_double 'TaxonomyParser' }
+  let(:output_dir) { '' }
+
+  subject(:subject) do
+    BatchProcessor.new({
+      destination_parser: destination_parser,
+      taxonomy_parser: taxonomy_parser,
+      renderer: renderer,
+      output_dir: output_dir
+    })
+  end
+
+  it { is_expected.to respond_to :renderer }
   it { is_expected.to respond_to :destination_parser }
   it { is_expected.to respond_to :taxonomy_parser }
+  it { is_expected.to respond_to :call }
 end
