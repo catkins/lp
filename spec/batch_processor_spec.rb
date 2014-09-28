5a3698b09dd58268dcbc9ec0ab3c16f6e3a70787
Chris Atkins <christopherlionelatkins@gmail.com>
2014-09-29 02:22:19 +1000

    filled in some more of the batch processor spec

--- spec/batch_processor_spec.rb
+++ spec/batch_processor_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe BatchProcessor do
-  let(:renderer) { instance_double 'Renderer' }
+  let(:renderer) { instance_double 'Renderer', render: 'Rendered Template' }
   let(:destination_parser) { instance_double 'DestinationParser' }
-  let(:taxonomy_parser) { instance_double 'TaxonomyParser' }
+  let(:taxonomy_parser) { instance_double 'TaxonomyParser', traverse: nil }
   let(:output_dir) { '' }
 
   subject(:subject) do
@@ -18,5 +18,17 @@ describe BatchProcessor do
   it { is_expected.to respond_to :renderer }
   it { is_expected.to respond_to :destination_parser }
   it { is_expected.to respond_to :taxonomy_parser }
-  it { is_expected.to respond_to :call }
+
+  describe '#call' do
+    let(:taxonomy_node) { instance_double 'TaxonomyNode', atlas_id: '1234' }
+    let(:destination) { instance_double 'Destination' }
+
+    before(:each) do
+      allow(taxonomy_parser).to receive(:traverse).and_yield taxonomy_node
+      allow(destination_parser).to receive(:find_by_atlas_id).and_return(destination)
+    end
+
+    it { is_expected.to respond_to :call }
+
+  end
 end
