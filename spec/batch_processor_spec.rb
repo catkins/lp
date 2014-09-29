23f096a00e7423190650c8ae6423ce39ff534b37
Chris Atkins <christopherlionelatkins@gmail.com>
2014-09-29 21:52:09 +1000

    filled in some specs for the batch processor class

--- spec/batch_processor_spec.rb
+++ spec/batch_processor_spec.rb
@@ -15,20 +15,39 @@ describe BatchProcessor do
     })
   end
 
-  it { is_expected.to respond_to :renderer }
-  it { is_expected.to respond_to :destination_parser }
-  it { is_expected.to respond_to :taxonomy_parser }
+  describe '#renderer' do
+    it { is_expected.to respond_to :renderer }
+    its(:renderer) { is_expected.to eq renderer }
+  end
+
+  describe '#destination_parser' do
+    it { is_expected.to respond_to :destination_parser }
+    its(:destination_parser) { is_expected.to eq destination_parser }
+  end
+
+  describe '#taxonomy_parser' do
+    it { is_expected.to respond_to :taxonomy_parser }
+    its(:taxonomy_parser) { is_expected.to eq taxonomy_parser }
+  end
 
   describe '#call' do
-    let(:taxonomy_node) { instance_double 'TaxonomyNode', atlas_id: '1234' }
-    let(:destination) { instance_double 'Destination' }
-
-    before(:each) do
-      allow(taxonomy_parser).to receive(:traverse).and_yield taxonomy_node
-      allow(destination_parser).to receive(:find_by_atlas_id).and_return(destination)
-    end
-
     it { is_expected.to respond_to :call }
 
+    let(:first_node) { instance_double 'TaxonomyNode', atlas_id: '1234' }
+    let(:second_node) { instance_double 'TaxonomyNode', atlas_id: '5678' }
+    let(:first_destination) { instance_double 'Destination' }
+    let(:second_destination) { instance_double 'Destination' }
+
+    before(:each) do
+      allow(taxonomy_parser).to receive(:traverse).and_yield(first_node).and_yield(second_node)
+      allow(destination_parser).to receive(:find_by_atlas_id).with('1234').and_return(first_destination)
+      allow(destination_parser).to receive(:find_by_atlas_id).with('5678').and_return(second_destination)
+    end
+
+    it 'yields the rendered template, and the current destination for each iteration of the taxonomy traversal' do
+      first_args  = [String, first_destination]
+      second_args = [String, second_destination]
+      expect { |b| subject.call(&b) }.to yield_successive_args(first_args, second_args)
+    end
   end
 end
